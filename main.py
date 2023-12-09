f84840da4032e229f6046a28b6438a405dec4dde
Archan Ghosh <gharchan@gmail.com>
2023-12-09 21:04:00 +0530

    Added Main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+Implementation of the Arxiv Summarizer as an independent module
+'''
+
+from langchain.document_loaders import ArxivLoader 
+from transformers import pipeline
+
+def doc_loader(search_query):
+    '''The purpose of this function is to load the documents from Arxiv'''
+    '''We are using the Arxiv Loader under Langchain library, which intern invokes the Arxiv API'''
+
+    try :
+        docs = ArxivLoader(query=search_query, load_max_docs=1).load()
+        return docs[0].metadata, docs[0].page_content
+    except Exception as e:
+        print(e)
