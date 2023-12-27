96bef9fbff977d315610209a598494b93a69adc2
Archan Ghosh <gharchan@gmail.com>
2023-12-27 05:03:51 +0000

    updated version from 0.1.0 to 0.1.1 in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="arxiv-summarizer",
-    version="0.1.0",
+    version="0.1.1",
     description="A happy toolkit for arxiv paper summarization and understanding.",
     long_description=long_description,
     long_description_content_type="text/markdown",
